3feb619b133cd9458af44505023d955b34feaa0f
hanmufu <hanmufu@gmail.com>
2020-03-13 18:09:46 -0400

    development done, next deal with std io

---
+++ main.py
@@ -0,0 +1,17 @@
+def check_mapping(s1:str, s2:str):
+    if len(s1) != len(s2):
+        print("two strings are not equal in length")
+        return False
+    dict = {}
+    for i in range(len(s1)):
+        c1 = s1[i]
+        c2 = s2[i]
+        if not c1 in dict:
+            dict[c1] = c2
+        else:
+            if dict[c1] != c2:
+                return False
+    return True
+
+if __name__ == "__main__":
+    print(check_mapping("aac", "a"))
